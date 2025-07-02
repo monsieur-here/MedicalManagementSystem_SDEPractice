6eb29d824d65f8f4133f86be6462e98d304631b4
Josh8al <josh8al@gmail.com>
2025-07-02 18:22:44 +0200

    added auth wrapper and completed authentication flow as per the user roles path access

---
+++ med-manage/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import AuthWrapper from "$lib/components/AuthWrapper.svelte";
+  import { AUTH_ROUTES } from "$lib/routes";
+</script>
+
+<AuthWrapper redirectTo={AUTH_ROUTES.login.url} let:user let:isAuthenticated>
+  {#if isAuthenticated}
+    <main>
+      <slot />
+    </main>
+  {/if}
+</AuthWrapper>

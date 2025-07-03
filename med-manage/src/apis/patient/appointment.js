0e1eea1089a88e1fbeb2c948f937ab92b016895f
Josh8al <josh8al@gmail.com>
2025-07-03 20:17:31 +0200

    patient appointment: added appointment post api

---
+++ med-manage/src/apis/patient/appointment.js
@@ -0,0 +1,28 @@
+import { BASE_API_URL } from "$lib/config";
+
+// Patient: POST: Appointment
+export const createAppointment = async (payload) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify(payload);
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      BASE_API_URL + "/patient/book-appointment",
+      requestOptions
+    );
+    const result = await response.text();
+    return result;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};

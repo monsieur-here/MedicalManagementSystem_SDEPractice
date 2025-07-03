f14396658978742e90a8542710c7677245f874b2
Josh8al <josh8al@gmail.com>
2025-07-04 01:29:18 +0200

    receptionist: added request for get appointment api

---
+++ med-manage/src/apis/receptionist/appointment.js
@@ -0,0 +1,27 @@
+//
+
+import { BASE_API_URL } from "$lib/config";
+
+// RECEPTIONIST: GET: get appointment
+export const getReceptionistAppointment = async () => {
+  const myHeaders = new Headers();
+  myHeaders.append("Cookie", "JSESSIONID=06E0804CBC82AF459E791FE8CAB0A83D");
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      BASE_API_URL + "/receptionist/appointments",
+      requestOptions
+    );
+    const result = await response.text();
+    return result;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};

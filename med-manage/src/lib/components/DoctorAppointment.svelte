9d271b5e48549181b28610a76ed0b68df4eaf491
Josh8al <josh8al@gmail.com>
2025-07-04 12:57:29 +0200

    doctor: prescription: added compact doctor appointment component

--- med-manage/src/lib/components/DoctorAppointment.svelte
+++ med-manage/src/lib/components/DoctorAppointment.svelte
@@ -1,4 +1,10 @@
 <script>
+  export let appointment;
+  export let hydrateRecords;
+
+  import { addPrescription } from "../../apis/doctor/appointment";
+  import { putReceptionistAppointment } from "../../apis/receptionist/appointment";
+
   let isOpen = false;
 
   let prescriptionName = "";
@@ -7,21 +13,76 @@
   let frequency = "";
   let criticality = "";
 
-  let loading = "";
+  let loading = false;
 
   function toggleBox() {
     isOpen = !isOpen;
   }
+
+  const handleAddPrescription = async () => {
+    loading = true;
+
+    const payload = {
+      appointment_id: appointment?.appointment?.id,
+      prescription_name: prescriptionName,
+      medication: medication,
+      dosage: dosage,
+      frequency: frequency,
+      criticality: criticality,
+    };
+
+    const res = await addPrescription(payload);
+
+    if (res === "Prescription addded successfully.") {
+      const date = new Date(appointment?.appointment?.appointmentDate);
+
+      // Helper function to pad 0s
+      const pad = (n) => n.toString().padStart(2, "0");
+
+      // Build formatted string
+      const formattedDate = `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
+
+      const updateApp = await putReceptionistAppointment({
+        status: "COMPLETED",
+        appointment_id: appointment?.appointment?.id,
+        appointment_date: formattedDate,
+      });
+
+      if (updateApp?.code === 200) {
+        alert(
+          "Prescription added successfully for " +
+            appointment?.patient?.first_name +
+            " " +
+            appointment?.patient?.last_name +
+            "!"
+        );
+
+        hydrateRecords();
+
+        // cache cleanup
+        isOpen = false;
+        prescriptionName = "";
+        medication = "";
+        dosage = "";
+        frequency = "";
+        criticality = "";
+      }
+    }
+
+    loading = false;
+  };
 </script>
 
-<button class="toggle-btn" on:click={toggleBox}>
-  {isOpen ? "Hide" : "Show"} Details
-</button>
+{#if appointment?.appointment?.appointmentDate && appointment?.appointment?.status !== "COMPLETED"}
+  <button class="toggle-btn" on:click={toggleBox}>
+    {isOpen ? "Hide" : "Show"} Details
+  </button>
+{/if}
 
 {#if isOpen}
   <div class="box">
     <form
-      on:submit|preventDefault={handleLogin}
+      on:submit|preventDefault={handleAddPrescription}
       class="card p-6 max-w-md mx-auto"
     >
       <label class="label">
@@ -78,7 +139,17 @@
           disabled={loading}
         />
       </label>
+
+      <div style="display: flex; justify-content: end;">
+        <button type="submit" class="login-button">Add Prescription</button>
+      </div>
     </form>
+  </div>
+{/if}
+
+{#if appointment?.appointment?.status === "REQUESTED"}
+  <div style="background-color: antiquewhite; border-radius: 4px;">
+    <p style="padding: 4px 8px;">Waiting for Appointment to be Scheduled</p>
   </div>
 {/if}
 
@@ -119,6 +190,14 @@
     border: none;
     padding: 0.5rem 1rem;
     border-radius: 6px;
+    cursor: pointer;
+  }
+  .login-button {
+    padding: 0.5rem;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 4px;
     cursor: pointer;
   }
 </style>
